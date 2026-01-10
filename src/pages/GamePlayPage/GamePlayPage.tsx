a14c23155dff0a72eb85479c1d80e1ae91fe063a
Karin Zohar <karinzohar3@gmail.com>
2026-01-10 15:43:26 +0200

    feat: style tile statuses

--- src/pages/GamePlayPage/GamePlayPage.tsx
+++ src/pages/GamePlayPage/GamePlayPage.tsx
@@ -5,7 +5,7 @@ import type { TileRowType, TileType } from "./GamePlayPage.type";
 type GamePlayPageProps = {};
 
 const GamePlayPage: FC<GamePlayPageProps> = ({}) => {
-  const emptyTile: TileType = { status: "empty" };
+  const emptyTile: TileType = { status: "empty", content: "" };
   const emptyRow: TileRowType = {
     tiles: Array.from({ length: 5 }, () => emptyTile),
     isActive: false,
