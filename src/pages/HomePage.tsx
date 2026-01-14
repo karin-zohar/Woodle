283bf91d5f6ae71e466ee5bbf39444f3528b9b03
Karin Zohar <karinzohar3@gmail.com>
2026-01-14 16:55:50 +0200

    feat: simplify home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,49 +2,22 @@ import { useGame } from "@/libs/hooks/useGame";
 import { Button, Typography } from "antd";
 import { useNavigate } from "react-router-dom";
 
-type GameStatus = "notStarted" | "inProgress" | "finished";
-
 const HomePage = () => {
   const navigate = useNavigate();
-  const { gameSettings, guesses } = useGame();
+  const { gameSettings } = useGame();
   const { wordLength } = gameSettings;
   const maxGuessAmount = wordLength + 1;
-  const guessAmount = guesses.length;
-  console.log("guessAmount:", guessAmount);
   const { Title } = Typography;
 
-  const gameStatus: GameStatus =
-    guessAmount > 0
-      ? guessAmount < maxGuessAmount
-        ? "inProgress"
-        : "finished"
-      : "notStarted";
-
-  const contentByStatus = {
-    notStarted: {
-      header: "Woodle",
-      subheader: `Get ${maxGuessAmount} chances to guess a ${
-        maxGuessAmount - 1
-      }-letter word.`,
-      button: "Play",
-    },
-    inProgress: {
-      header: "Welcome Back",
-      subheader: `You've made ${guessAmount} out of ${maxGuessAmount} guesses. Keep it up!`,
-      button: "Continue",
-    },
-    finished: {
-      header: "Hi Woodler",
-      subheader: "Game over",
-      button: "Play Again",
-    },
+  const content = {
+    header: "Woodle",
+    subheader: `Get ${maxGuessAmount} chances to guess a ${
+      maxGuessAmount - 1
+    }-letter word.`,
+    button: "Play",
   };
 
-  const {
-    header,
-    subheader,
-    button: buttonContent,
-  } = contentByStatus[gameStatus];
+  const { header, subheader, button: buttonContent } = content;
   return (
     <div className="home-page">
       <Title level={1}>{header}</Title>
