42de6e0e21e739f4505e1e09b177e345b661b004
Karin Zohar <karinzohar3@gmail.com>
2026-02-09 15:04:20 +0200

    feat: persist theme using local storage

--- src/store/slices/theme.slice.tsx
+++ src/store/slices/theme.slice.tsx
@@ -1,14 +1,21 @@
 import { type StateCreator } from "zustand";
 
+const THEME_LOCAL_STORAGE_KEY = "theme";
+
 type Theme = "light" | "dark";
 export type ThemeSlice = {
   theme: Theme;
   setTheme: (theme: Theme) => void;
 };
 
-export const themeSlice: StateCreator<ThemeSlice> = (set) => ({
-  theme: "light",
-  setTheme: (theme: Theme) => {
-    set({ theme });
-  },
-});
+export const themeSlice: StateCreator<ThemeSlice> = (set, get) => {
+  const initialTheme = (localStorage.getItem(THEME_LOCAL_STORAGE_KEY) as Theme) || "light";
+  
+  return {
+    theme: initialTheme,
+    setTheme: (theme: Theme) => {
+      set({ theme });
+      localStorage.setItem(THEME_LOCAL_STORAGE_KEY, theme);
+    },
+  };
+};
